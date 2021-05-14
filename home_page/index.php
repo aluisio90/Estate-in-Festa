5c86d582a5e9862db713e64ab6cbd2a7a22a5e6b
Andrea Pizzi <andreapizzi90@outlook.com>
2021-05-14 23:30:03 +0200

    create style index page

---
+++ home_page/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Foundation | Welcome</title>
+  <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+</head>
+
+<body>
+
+  <!-- Start Top Bar -->
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">Musicando Corp.</li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="#">Negozio</a></li>
+        <li><a href="#">Spettacoli</a></li>
+        <li><a href="#">Contatti</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- End Top Bar -->
+
+  <div class="callout large primary">
+    <div class="row column text-center">
+      <h1>Un Estate in Musica</h1>
+      <h5><i>La grande musicale della tua estate</i></h5>
+    </div>
+  </div>
+
+  <div class="row" id="content">
+    <div class="medium-8 columns">
+      <div class="blog-post">
+        <h3>Acquista adesso un biglietto per un'estate piena di musica <small><?= date("j-n-Y"); ?></small></h3>
+        <img class="thumbnail" src="resurce/logo.png">
+        <p>
+          <h1 id="sunnye-arrivata-finalmente-lestatesunny">☀️E’ Arrivata finalmente l’estate☀️</h1>
+          <h3 id="scegli-solo-la-migliore-musica-per-unestate--unica">Scegli solo la migliore musica per un’estate unica</h3>
+          <p>Scopri le nostre offerte su centinaia di concerti per riempire di musica la tua estate!</p>
+          <p>Sfoglia la nostra pagina <a href="">eventi</a> per scoprire i concerti in programmazione 🎼</p>
+          <p>Vai nella sezione <a href="">negozio</a> per scegliere il biglietto d’acquistare.</p>
+          <p>Acquista il biglietto e attendi l’arrivo di un email ✉️ all’indirizzo di posto che hai indicato, il tuo biglietto sarà allegato al messaggio!!</p>
+          <p><strong>E non preoccuparti se non ti sarà recapitata nessuna email per un errore nell’indirizzo email non ti sarà addebitato nessuno costo</strong>‼️</p>
+          <p>Viva un’estate migliore con la musica, compra adesso il biglietto per un’estate indimenticabile. ⛵️</p>
+          <p><em><strong>Musicando, la grande musica dalla A alla Z</strong></em></p>
+          <p><em>ps per contattarci visita la pagina <a href="">contatti</a> sia a tua disposizione</em>😄😄</p>
+        </p>
+        <div class="callout">
+          <ul class="menu simple">
+            <li><a href="estate.in.musica@musicando.it">Autore: Admin</a></li>
+          </ul>
+        </div>
+      </div>
+</body>
+
+</html>
