31e12ea80c7d44cf86ab71b30cf446f8d111f392
Andrea Pizzi <andreapizzi90@outlook.com>
2021-05-15 00:20:26 +0200

    create contatti page

---
+++ home_page/contatti/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="it">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Musicando | Home</title>
+  <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+</head>
+
+<body>
+
+  <!-- Start Top Bar -->
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">Musicando Corp.</li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="negozio/">Negozio</a></li>
+        <li><a href="spettacoli/">Spettacoli</a></li>
+        <li><a href="contatti/">Contatti</a></li>
+        <li><a href="login/">Login</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- End Top Bar -->
+
+  <div class="callout large primary">
+    <div class="row column text-center">
+      <h1>Un Estate in Musica</h1>
+      <h5><i>La grande musicale della tua estate</i></h5>
+    </div>
+  </div>
+
+  <div class="row" id="content">
+    <div class="medium-8 columns">
+      <div class="blog-post">
+        <h3><!-- mettere qualcosa qui .... --></h3>
+        <img class="thumbnail" src="resurce/logo.png">
+        <p>
+        </p>
+        <div class="callout">
+          <ul class="menu simple">
+            <li><i><strong><div style="color:rgb(0, 153, 255);">Email</div></strong> estate.in.festa.help@musicando.it</i></li><br>
+            <li><i><strong><div style="color:rgb(0, 153, 255);">Numero di telefono</div></strong>+39 446 778 1978</i></li>
+          </ul>
+        </div>
+      </div>
+</body>
+
+</html>
